422abb8fd3dfc58fe8cc85981daa8534e449803e
Yasyf Mohamedali <yasyfm@gmail.com>
2014-12-29 19:04:37 -0800

    Force SSL in production

--- bcferries_web/routes/main.py
+++ bcferries_web/routes/main.py
@@ -1,12 +1,18 @@
-from flask import render_template
-from bcferries_web import app
+from flask import render_template, request, redirect
+from bcferries_web import app, DEV
 
 @app.before_request
 def preprocess_request():
-  pass
+  if not DEV:
+    https_indicators = ['CF-Visitor', 'X-Forwarded-Proto']
+    if not any(['https' in x for x in https_indicators]):
+      url = request.url.replace('http://', 'https://', 1)
+      return redirect(url, code=301)
 
 @app.after_request
 def postprocess_request(response):
+  if not DEV:
+    response.headers.setdefault('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
   return response
 
 @app.route('/')
