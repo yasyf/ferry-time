501a4c73c6305ac863070ad1104204c2a82c88dc
Yasyf Mohamedali <yasyfm@gmail.com>
2015-01-02 00:57:16 -0800

    Preload workers

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ then
   python bcferries_web/app.py
 else
   python bcferries_web/clock/run.py &
-  gunicorn -b "0.0.0.0:$PORT" -w 5 -k eventlet bcferries_web.app:app
+  gunicorn -b "0.0.0.0:$PORT" -w 5 -k eventlet --preload bcferries_web.app:app
 fi
