3cf15e4f00e627a2e818c501772d2c440def6e3d
Yasyf Mohamedali <yasyfm@gmail.com>
2015-01-01 21:24:00 -0800

    Add print statements

--- bcferries_web/clock/run.py
+++ bcferries_web/clock/run.py
@@ -1,13 +1,14 @@
 from bcferries_web import scheduler, lock
-import lockfile
+import lockfile, threading
 
 from clock import *
 
 try:
   lock.acquire(timeout=1)
+  print "Running scheduler in thread {}".format(threading.current_thread().ident)
   scheduler.start()
 except lockfile.LockTimeout:
-  print "Could not aquire scheduler lock!"
+  print "Could not aquire scheduler lock on thread {}".format(threading.current_thread().ident)
 finally:
   if lock.i_am_locking():
     lock.release()
